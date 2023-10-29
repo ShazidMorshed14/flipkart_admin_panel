030868dbf1b3543799b9b703db3e244983326b86
ShazidMorshed14 <shazidmorshed14@gmail.com>
2023-10-30 01:49:07 +0600

    mantine table package added

--- src/pages/auth/UserManagement.jsx
+++ src/pages/auth/UserManagement.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import MantineCrudTable from "../global/MantineCrudTable";
 
 const UserManagement = () => {
-  return <div>UserManagement</div>;
+  return (
+    <div>
+      <MantineCrudTable />
+    </div>
+  );
 };
 
 export default UserManagement;
